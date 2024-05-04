97bb040123dc6f5646a9cac78e2f0b0ce5cc1c8b
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-05-04 15:59:17 +0800

    correct index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,41 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './components/PhonebookRedux/store';
-import { App } from './components/App'; // Corrected import statement
+import { App } from './components/App';
 import './index.css';
+
+const Loading = () => <div>Loading...</div>; // Simple Loading component
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // Log error to error reporting service here
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
